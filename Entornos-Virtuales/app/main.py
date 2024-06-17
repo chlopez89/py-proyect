882119790724b808ed6c85276707b3f0792db45b
{Christian López} <chlopez89@gmail.com>
2024-06-16 22:27:49 -0600

    Instalación de requests para la web y como utilizar las librerias de pandas

--- Entornos-Virtuales/app/main.py
+++ Entornos-Virtuales/app/main.py
@@ -2,6 +2,7 @@ import utils
 import read_csv
 import charts
 import os
+import pandas as pd
 
 def run():
     
@@ -12,18 +13,35 @@ def run():
     'Europe','\n',
     'North America','\n',
     'South America','\n')
+  
   continent = input('Type Continent => ')
-  data = read_csv.read_csv('./data.csv')
-  data = list(filter(lambda item : item['Continent'] == continent,data))
-
-  countries = list(map(lambda x: x['Country'], data))
-  percentages = list(map(lambda x: x['World Population Percentage'], data))
+  
+  '''
+  -> Comentado para utilizar las librerias de Panda 
+    data = read_csv.read_csv('./data.csv')
+    data = list(filter(lambda item : item['Continent'] == continent,data))
+    
+    countries = list(map(lambda x: x['Country'], data))
+    percentages = list(map(lambda x: x['World Population Percentage'], data))
+  '''
+  # Con las librerias de Pandas.
+  df = pd.read_csv('./data.csv')
+  df = df[df['Continent'] == continent]
+  # print(type(df))
+  # print(df)
+  
+  countries = df['Country'].values
+  percentages = df['World Population Percentage'].values
+  
   print('Contry =>')
   print(*countries, sep='\n')
   country = input('Type Country => ')
   print(f'Country seleccionado {country}')
-  result = utils.population_by_country(data, country)
+  result = utils.population_by_country(df, country)
 
+  # poblacion_df = df[df['Country']==country]
+  # print('Población: ','\n',poblacion_df)
+  
   bar, pie=f'./imgs/{country}_bar.png', f'./imgs/{continent}_pie.png'
   if os.path.exists(bar):
     os.remove(bar)
@@ -31,8 +49,9 @@ def run():
     os.remove(pie)
   charts.generate_pie_chart(countries, percentages, continent)
   if len(result) > 0:
-    country_data = result[0]
-    labels, values = utils.get_population(country_data)
+    # country_data = result[0]
+    labels, values = utils.get_population(result)
     charts.generate_bar_chart(labels, values, country)
+
 if __name__ == '__main__':
   run()
--- Entornos-Virtuales/app/utils.py
+++ Entornos-Virtuales/app/utils.py
@@ -1,19 +1,24 @@
-def get_population(country_dict):
+import pandas as pd
+def get_population(df):
+  # print(type(df))
+  # print('Dato 2022: ')
+  # print(int(df['2022 Population'].values))
   population_dict = {
-      '2022': int(country_dict['2022 Population']),
-      '2020': int(country_dict['2020 Population']),
-      '2015': int(country_dict['2015 Population']),
-      '2010': int(country_dict['2010 Population']),
-      '2000': int(country_dict['2000 Population']),
-      '1990': int(country_dict['1990 Population']),
-      '1980': int(country_dict['1980 Population']),
-      '1970': int(country_dict['1970 Population'])
+      '2022': int(df['2022 Population'].values),
+      '2020': int(df['2020 Population'].values),
+      '2015': int(df['2015 Population'].values),
+      '2010': int(df['2010 Population'].values),
+      '2000': int(df['2000 Population'].values),
+      '1990': int(df['1990 Population'].values),
+      '1980': int(df['1980 Population'].values),
+      '1970': int(df['1970 Population'].values)
   }
   labels = population_dict.keys()
   values = population_dict.values()
   return labels, values
 
 
-def population_by_country(data, country):
-  result = list(filter(lambda item: item['Country'] == country, data))
+def population_by_country(df, country):
+  # result = list(filter(lambda item: item['Country'] == country, data))
+  result = df[df['Country']==country]
   return result
