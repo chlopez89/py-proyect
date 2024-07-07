3edbb8197dfe4feb7d25be9055f2ed428c20cc9a
{Christian López} <chlopez89@gmail.com>
2024-07-06 18:20:46 -0600

    # Creación de los Dockerfile - Configuracion de los Dockerfile - modificaciones a requirements de la parte web.

--- Entornos-Virtuales/web-server/main.py
+++ Entornos-Virtuales/web-server/main.py
@@ -1,4 +1,28 @@
 import store
+# from fastapi import FastApi
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1,2,3]
+
+@app.get('/contact',response_class=HTMLResponse)
+async def get_list():
+    return """
+        <html>
+            <head>
+                <title>FastAPI - Christian</title>
+            </head>
+            <body>
+                <h1>Datos de contacto</h1>
+                <h2> Numero de contaco - 43432343298742 </h2>
+            </body>
+        </html>
+        """
+
 
 def run():
     store.get_categories()
