baaf0fef01ac5e14564992c73fc8bef1c790010e
{Christian López} <chlopez89@gmail.com>
2025-01-13 23:21:08 -0600

    Modificaciones para las pruebas del bank_account

--- py-testing/tests/test_api_client.py
+++ py-testing/tests/test_api_client.py
@@ -1,4 +1,4 @@
-import unittest
+import unittest, requests
 from src.api_client import get_location
 from unittest.mock  import patch
 
@@ -18,3 +18,28 @@ class ApiClientTests(unittest.TestCase):
         self.assertEqual(result.get("city"), "Leon")
 
         mock_get.assert_called_once_with("https://freeipapi.com/api/json/8.8.8.8")
+
+
+    @patch('src.api_client.requests.get')
+    def test_get_location_returns_side_effect(self, mock_get):
+        mock_get.side_effect = [
+            requests.exceptions.RequestException("Services Unavailable"),
+            unittest.mock.Mock(
+                status_code=200,
+                json=lambda: {
+                    "country": "Mexico",
+                    "region": "Guanajuato",
+                    "city": "Leon",
+                }
+            )
+        ]
+        with self.assertRaises(requests.exceptions.RequestException):
+            get_location("8.8.8.8")
+
+
+        # result = get_location("8.8.8.8")
+        # self.assertEqual(result.get("country"), "Mexico")
+        # self.assertEqual(result.get("region"), "Guanajuato")
+        # self.assertEqual(result.get("city"), "Leon")
+
+        # result = get_location("8.8.8.8")
