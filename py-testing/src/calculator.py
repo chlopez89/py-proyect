8b59efbd6fdfc53ce51623613bc82aea424cdb31
{Christian López} <chlopez89@gmail.com>
2025-01-14 00:09:36 -0600

    Creacion de la clase user y se creo el archivo para las pruebas

--- py-testing/src/calculator.py
+++ py-testing/src/calculator.py
@@ -1,13 +1,40 @@
 def sum(a, b):
+    """
+    >>> sum(5, 7)
+    12
+
+    >>> sum(5, 5)
+    10
+
+    >>> sum(5, -4)
+    1
+    """
     return a + b
 
+
 def subtract(a, b):
+    """
+    >>> subtract(10, 5)
+    5
+    """
+
     return a - b
 
+
 def multiply(a, b):
+    """
+    >>> multiply(2, 5)
+    10
+    """
     return a * b
 
+
 def divide(a, b):
+    """
+    >>> divide(10, 0)
+    Traceback (most recent call last):
+    ValueError: La división por cero no está permitida
+    """
     if b == 0:
         raise ValueError("La división por cero no está permitida")
     return a / b
