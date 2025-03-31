b3c7da04a91959a582fd1f4615ea9027972f4a1b
{Christian López} <chlopez89@gmail.com>
2025-03-30 23:41:15 -0600

    Update de hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,5 @@
-print("Hola desde Python")
+# Este código imprime un mensaje de saludo en la consola.
+# Se ha modificado para incluir un input que solicita el nombre del usuario.
+
+nombre = input("Por favor, ingresa tu nombre: ")
+print(f"Hola, {nombre}, desde Python")
