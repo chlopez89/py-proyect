4997b1d3c4b8eb43fe8ae9a21faec1a68eb4f9c3
Christian Lopez <chlopez89@gmail.com>
2026-01-23 17:07:24 -0600

    Modificacion del archivo holaMundo.py

--- Curso de Python/holaMundo.py
+++ Curso de Python/holaMundo.py
@@ -1,2 +1,12 @@
+def presentacion():
+    print("Hola Mundo")
+    print("Bienvenido al curso de Python")
+    Nombre=input("¿Cuál es tu nombre? ")
+    Edad=input("¿Cuántos años tienes? ")
+    if Edad.isdigit() and int(Edad) < 18:
+        print("Por favor, eres menor de edad o no has ingresado un número válido.")
+    else:
+        print(f"Hola {Nombre}, tienes {Edad} años.")
 
-print("Hola Mundo")
+presentacion()
+print(f"fin del programa")
