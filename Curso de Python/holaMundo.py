53f237a0677ce1b9baa4687afd1c483e950f5626
Christian Lopez <chlopez89@gmail.com>
2026-05-06 13:48:46 -0600

    Modicaciones del holaMundo.py

--- Curso de Python/holaMundo.py
+++ Curso de Python/holaMundo.py
@@ -1,12 +1,22 @@
+from datetime import datetime
+
+
 def presentacion():
     print("Hola Mundo")
     print("Bienvenido al curso de Python")
-    Nombre=input("¿Cuál es tu nombre? ")
-    Edad=input("¿Cuántos años tienes? ")
-    if Edad.isdigit() and int(Edad) < 18:
-        print("Por favor, eres menor de edad o no has ingresado un número válido.")
+    nombreUsuario=input("¿Cuál es tu nombre? ")
+    annoNacimiento=input("¿En que año naciste? ")
+    mesNacimiento=input("¿En que mes naciste?(ingresa el numero de mes) ")
+    Hoy=datetime.now()
+    if annoNacimiento.isdigit() and mesNacimiento.isdigit():
+        edad = Hoy.year - int(annoNacimiento)
+        edad -= 1 if (Hoy.month, Hoy.day) < (int(mesNacimiento), 1) else 0
+        if edad < 18:
+            print("Por favor, eres menor de edad o no has ingresado un número válido.")
+        else:
+            print(f"Hola {nombreUsuario}, tienes {edad} años.")
     else:
-        print(f"Hola {Nombre}, tienes {Edad} años.")
+        print("Por favor, ingresa un año de nacimiento válido.")
 
 presentacion()
 print(f"fin del programa")
